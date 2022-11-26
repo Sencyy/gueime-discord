88a29df0b08ee35530ddcd5fd043ff5de1ba7e61
Sencyy <l1.nx@outlook.com>
2022-11-26 14:58:32 -0300

    modified:   .gitignore 	new file:   commands/avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,13 @@
+const {SlashCommandBuilder} = require("discord.js");
+const { client } = require("tenorjs");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("avatar")
+        .setDescription("Manda o download do seu avatar."),
+    async execute(interaction) {
+        await interaction.reply(interaction.user.displayAvatarURL());
+        console.log("Avatar solicitado!");
+        
+    },
+};
